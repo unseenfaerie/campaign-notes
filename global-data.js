b466fe11c6e3b8ea2153820145d3afac0be85f28
unseenfaerie <unseenfaerie@protonmail.com>
2025-08-15 19:48:57 -0500

    added global data.js

---
+++ global-data.js
@@ -0,0 +1,26 @@
+window.CAMPAIGN = {
+    characters: [
+        {
+            id: "alann-barnett",
+            name: "Alann Barnett",
+            aliases: ["Alann Barnett", "Alann"],
+            href: "/characters/player-characters/alann-barnett.html",
+            class: "Fighter",
+            level: 3,
+            alignment: "Lawful Good",
+            // ...more fields
+        },
+        // ...more characters
+    ],
+    places: [
+        {
+            id: "wavethorn",
+            name: "Wavethorn",
+            aliases: ["Wavethorn"],
+            href: "/places/othlorin/wavethorn/wavethorn.html",
+            // ...more fields
+        },
+        // ...more places
+    ],
+    // Add more entity types as needed
+};
