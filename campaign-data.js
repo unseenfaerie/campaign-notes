cefd26c88c24a281cee1f48235d48ef143222340
unseenfaerie <unseenfaerie@protonmail.com>
2025-08-17 11:03:35 -0500

    created campaign-data.js

---
+++ campaign-data.js
@@ -0,0 +1,209 @@
+//Create global data structure containing playerCharacters, nonPlayerCharacters, places, items, sessions, organizations, and deities
+const campaignData = {
+    playerCharacters: [],
+    nonPlayerCharacters: [],
+    deities: [],
+    organizations: [],
+    places: [],
+    items: [],
+    sessions: []
+};
+campaignData.playerCharacters = [
+    {
+        id: "alann-barnett",
+        name: "Alann Barnett",
+        aliases: [
+            "Alann Barnett",
+            "Alann"
+        ],
+        statblock: {
+            "strength": 13,
+            "dexterity": 8,
+            "constitution": 11,
+            "intelligence": 10,
+            "wisdom": 14,
+            "charisma": 11
+        },
+        href: "/characters/player-characters/alann-barnett.html"
+    },
+    {
+        id: "releas-neb",
+        name: "Releas Neb",
+        aliases: [
+            "Releas Neb",
+            "Rel",
+            "Releas Scarnewman",
+            "Releas"
+        ],
+        statblock: {
+            strength: 5,
+            dexterity: 14,
+            constitution: 10,
+            intelligence: 18,
+            wisdom: 13,
+            charisma: 9
+        },
+        href: "/characters/player-characters/releas-neb.html"
+    },
+    {
+        id: "appolonia-palleday",
+        name: "Appolonia Palleday",
+        aliases: [
+            "Appolonia Palleday",
+            "Appolonia",
+            "Polly"
+        ],
+        statblock: {
+            strength: 13,
+            dexterity: 8,
+            constitution: 11,
+            intelligence: 18,
+            wisdom: 14,
+            charisma: 11
+        },
+        href: "/characters/player-characters/appolonia-palleday.html"
+    },
+    {
+        id: "durchir",
+        name: "Durchir of the Angry Orchard",
+        aliases: [
+            "Durchir of the Angry Orchard",
+            "Durchir"
+        ],
+        statblock: {
+            strength: 18,
+            dexterity: 10,
+            constitution: 12,
+            intelligence: 15,
+            wisdom: 10,
+            charisma: 11
+        },
+        href: "/characters/player-characters/durchir.html"
+    },
+    {
+        id: "cormac",
+        name: "Cormac",
+        aliases: [
+            "Cormac"
+        ],
+        statblock: {
+            strength: 9,
+            dexterity: 16,
+            constitution: 7,
+            intelligence: 15,
+            wisdom: 14,
+            charisma: 7
+        },
+        href: "/characters/player-characters/cormac.html"
+    }
+];
+campaignData.nonPlayerCharacters = [
+    {
+        id: "bert-verinwort",
+        name: "Bert Verinwort",
+        aliases: [
+            "Bert Verinwort",
+            "Bert"
+        ],
+        href: "/characters/non-player-characters/bert-verinwort.html"
+    },
+    {
+        id: "gereg",
+        name: "Gereg",
+        aliases: [
+            "Gereg"
+        ],
+        href: "/characters/non-player-characters/gereg.html"
+    },
+    {
+        id: "leo",
+        name: "Leo",
+        aliases: [
+            "Leo"
+        ],
+        href: "/characters/non-player-characters/leo.html"
+    }
+];
+campaignData.deities = [
+    {
+        id: "achiel",
+        name: "Achiel",
+        aliases: [
+            "Achiel",
+            "Achiel, God of Light",
+            "Achiel God of Light"
+        ],
+        href: "/characters/deities/achiel.html"
+    },
+    {
+        id: "idona",
+        name: "Idona",
+        aliases: [
+            "Idona",
+            "Idona, Goddess of Humanity",
+            "Idona, Goddess of the Moon"
+        ],
+        href: "/characters/deities/idona.html"
+    }
+];
+campaignData.organizations = [
+    {
+        id: "church-of-achiels-light",
+        name: "Church of Achiel's Light",
+        aliases: [
+            "Church of Achiel's Light",
+            "Church of Achiel",
+            "High Church"
+        ],
+        href: "/organizations/church-of-achiels-light.html"
+    }
+];
+campaignData.places = [
+    {
+        id: "anash",
+        name: "Anash",
+        aliases: [
+            "Anash"
+        ],
+        href: "/places/othlorin/itholis/weinmere/anash.html"
+    },
+    {
+        id: "wavethorn",
+        name: "Wavethorn",
+        aliases: [
+            "Wavethorn"
+        ],
+        href: "/places/othlorin/wavethorn-area/wavethorn.html"
+    },
+    {
+        id: "niba",
+        name: "Niba",
+        aliases: [
+            "Niba"
+        ],
+        href: "/places/othlorin/niba/niba.html"
+    }
+];
+campaignData.items = [
+    {
+        id: "cormac-spellbook",
+        name: "Cormac's Spellbook",
+        aliases: [
+            "Cormac's Spellbook"
+        ],
+        href: "/items/tomes/cormac-spellbook.html"
+    }
+];
+campaignData.sessions = [
+    {
+        id: "ootid-session-1-and-2",
+        name: "The Coup of Wavethorn",
+        aliases: [
+            "Coup of Wavethorn",
+            "Coup of 200",
+            "Wavethorn Coup",
+            "OOTID Sessions 1 and 2"
+        ],
+        href: "/sessions/order-of-the-iron-duch/session-1-and-2.html"
+    }
+];
