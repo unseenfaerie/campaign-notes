58bc5066d0ac520a8c88f2d008693d41b24c23bb
unseenfaerie <unseenfaerie@protonmail.com>
2025-08-17 18:12:41 -0500

    tryin to load from js

--- load-campaign-data.js
+++ load-campaign-data.js
@@ -1,8 +1,29 @@
+
+// 1. Load CAMPAIGN data from global-data.js
+
+
+const globalDataJs = fs.readFileSync(path.join(ROOT, 'campaign-data.js'), 'utf8');
+
+const campaignMatch = globalDataJs.match(/window\.CAMPAIGN\s*=\s*({[\s\S]*?});/);
+
+if (!campaignMatch) {
+    console.error('Could not find CAMPAIGN object in campaign-data.js');
+    process.exit(1);
+}
+
+// run campaign-data.js
+eval('(' + campaignMatch[1] + ')'); // Safe if you control the file
+
+
+const campaign = eval('(' + campaignMatch[1] + ')'); // Safe if you control the file
+
 (function () {
-    fetch('/campaign-data.json')
-        .then(response => response.json())
-        .then(data => {
-            window.CAMPAIGN = data;
+    //load campaign data from campaign-data.js
+    fetch('/campaign-data.js')
+        .then(response => response.text())
+        .then(text => {
+            eval(text);
+            window.CAMPAIGN = campaignData;
             // Dynamically load links.js after data is ready
             var script = document.createElement('script');
             script.src = '/links.js';
