4eb328b7403b86077e1fbe68071c966fb9a246a7
unseenfaerie <unseenfaerie@protonmail.com>
2025-08-23 19:15:06 -0500

    added axios

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,1 +1,9 @@
+import axios from 'axios';
+
 export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || 'http://10.0.0.215:3000/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL
+});
+
+export default api;
