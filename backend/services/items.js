7a29607cd16f2113d89dd6f1077a1fbf1d4e91ce
unseenfaerieservant <unseenfaerie@protonmail.com>
2025-08-24 17:53:13 -0500

    build out service layer

--- backend/services/items.js
+++ backend/services/items.js
@@ -1,15 +1,50 @@
-const db = require('../db');
+const dbUtils = require('../utils/dbUtils');
 
-// Get item details by item_id
-function getItemDetails(item_id) {
-  return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM items WHERE id = ?', [item_id], (err, row) => {
-      if (err) return reject(err);
-      resolve(row);
-    });
-  });
+const TABLE = 'items';
+
+// Create a new item
+function createItem(item) {
+  return dbUtils.insert(TABLE, item);
+}
+
+// Get all items
+function getAllItems() {
+  return dbUtils.select(TABLE);
+}
+
+// Get an item by id
+function getItemById(id) {
+  return dbUtils.select(TABLE, { id }, true);
+}
+
+// Update an item (full update)
+function updateItem(id, item) {
+  // Only update allowed fields (excluding id)
+  const allowed = ['name', 'short_description', 'long_explanation'];
+  const updates = {};
+  for (const key of allowed) {
+    if (item[key] !== undefined) updates[key] = item[key];
+  }
+  return dbUtils.update(TABLE, { id }, updates);
+}
+
+// Patch (partial update) an item
+function patchItem(id, updates) {
+  const allowed = ['name', 'short_description', 'long_explanation'];
+  const filtered = Object.fromEntries(Object.entries(updates).filter(([k]) => allowed.includes(k)));
+  return dbUtils.update(TABLE, { id }, filtered);
+}
+
+// Delete an item
+function deleteItem(id) {
+  return dbUtils.remove(TABLE, { id });
 }
 
 module.exports = {
-  getItemDetails
+  createItem,
+  getAllItems,
+  getItemById,
+  updateItem,
+  patchItem,
+  deleteItem
 };
