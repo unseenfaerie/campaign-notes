4620b6f340aefa06e178e9b273bd5bdbb3744ad3
unseenfaerieservant <unseenfaerie@protonmail.com>
2025-08-24 17:52:55 -0500

    fixed misreference

--- backend/services/characterDeities.js
+++ backend/services/characterDeities.js
@@ -1,39 +1,39 @@
 // services/characterDeities.js
 // Centralized logic for managing character-deity relationships
-const dbHelpers = require('./dbHelpers');
+const dbUtils = require('../utils/dbUtils');
 
 const TABLE = 'character_deities';
 
 // Add a character-deity relationship
 function addCharacterDeity(character_id, deity_id, short_description = '', long_explanation = '') {
-  return dbHelpers.insert(TABLE, { character_id, deity_id, short_description, long_explanation });
+  return dbUtils.insert(TABLE, { character_id, deity_id, short_description, long_explanation });
 }
 
 // Get all deities for a character (join table only)
 function getDeitiesForCharacter(character_id) {
-  return dbHelpers.select(TABLE, { character_id });
+  return dbUtils.select(TABLE, { character_id });
 }
 
 // Get all characters for a deity (join table only)
 function getCharactersForDeity(deity_id) {
-  return dbHelpers.select(TABLE, { deity_id });
+  return dbUtils.select(TABLE, { deity_id });
 }
 
 // Get a specific character-deity relationship (with join table metadata)
 function getCharacterDeity(character_id, deity_id) {
-  return dbHelpers.select(TABLE, { character_id, deity_id }, true);
+  return dbUtils.select(TABLE, { character_id, deity_id }, true);
 }
 
 // Update a character-deity relationship
 function updateCharacterDeity(character_id, deity_id, updates) {
   const allowed = ['short_description', 'long_explanation'];
   const filtered = Object.fromEntries(Object.entries(updates).filter(([k]) => allowed.includes(k)));
-  return dbHelpers.update(TABLE, { character_id, deity_id }, filtered);
+  return dbUtils.update(TABLE, { character_id, deity_id }, filtered);
 }
 
 // Remove a character-deity relationship
 function removeCharacterDeity(character_id, deity_id) {
-  return dbHelpers.remove(TABLE, { character_id, deity_id });
+  return dbUtils.remove(TABLE, { character_id, deity_id });
 }
 
 module.exports = {
