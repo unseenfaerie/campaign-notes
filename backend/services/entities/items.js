869dc77cbf2119bec449b65a4a554eba1427137c
unseenfaerie <unseenfaerie@protonmail.com>
2025-09-06 00:30:21 -0500

    replace basic item service with entity service

--- backend/services/entities/items.js
+++ backend/services/entities/items.js
@@ -1,31 +1,29 @@
-const dbUtils = require('../../utils/dbUtils');
-const serviceUtils = require('../../utils/serviceUtils');
+const entityDataService = require('../entityDataService');
 const fullEntityService = require('../fullEntityService');
+const entityName = 'Item';
 
-const TABLE = 'items';
-
-function createItem(item) {
-  return dbUtils.insert(TABLE, item);
+function createItem(data) {
+  return entityDataService.createEntity(entityName, data);
 }
 
 function getAllItems() {
-  return dbUtils.select(TABLE);
+  return entityDataService.getAllEntities(entityName);
 }
 
 function getItemById(id) {
-  return dbUtils.select(TABLE, { id }, true);
+  return entityDataService.getEntityById(entityName, id);
 }
 
-async function getFullItemById(id) {
-  return fullEntityService.getFullEntityById('Item', id);
+function getFullItemById(id) {
+  return fullEntityService.getFullEntityById(entityName, id);
 }
 
 function patchItem(id, updates) {
-  return serviceUtils.updateWithChangedFields(TABLE, { id }, updates);
+  return entityDataService.patchEntity(entityName, id, updates);
 }
 
 function deleteItem(id) {
-  return dbUtils.remove(TABLE, { id });
+  return entityDataService.deleteEntity(entityName, id);
 }
 
 module.exports = {
