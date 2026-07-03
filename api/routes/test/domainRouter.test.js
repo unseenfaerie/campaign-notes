cca9981ee9ac08094f8074cd319b2f4340f4098f
faerie <unseenfaerie@protonmail.com>
2026-07-02 22:41:41 -0500

    added test directory

--- api/routes/test/domainRouter.test.js
+++ api/routes/test/domainRouter.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
-const { app } = require('./server');
-const { initializeDatabase } = require('./db');
-const { manifestCrudService } = require('./genericCrudService');
+const { app } = require('../../server');
+const { initializeDatabase } = require('../../db');
+const { manifestCrudService } = require('../../genericCrudService');
 
 describe('domain router entity routes', () => {
     beforeAll(async () => {
