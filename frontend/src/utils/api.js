0012ffd0523edd95d6e4c1084a79c9f616a064f4
faerie <unseenfaerie@protonmail.com>
2026-07-05 20:26:35 -0500

    corrected base url in frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || 'http://10.0.0.215:3000/api';
+export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || 'http://localhost:3001/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL
