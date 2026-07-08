861d59cae97186c911d13df3bd2086ef827527df
faerie <unseenfaerie@protonmail.com>
2026-07-08 11:58:41 -0500

    ripped out old frontend

--- frontend/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || 'http://localhost:3001/api';
-
-const api = axios.create({
-  baseURL: API_BASE_URL
-});
-
-export default api;
